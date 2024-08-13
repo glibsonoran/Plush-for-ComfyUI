77eac2ce0c2eb3428a686e71a49de9ab6d0d4c31
glibsonoran <31249593+glibsonoran@users.noreply.github.com>
2024-08-13 16:55:45 -0700

    Add Ollama model field

--- __init__.py
+++ __init__.py
@@ -18,7 +18,7 @@ if jmanager.on_startup(False):
 else:
     jmanager.log_events("config.json was not updated")
 
-__version__ ="1.21.11"
+__version__ ="1.21.12"
 print('Plush - Version:', __version__)
 
 
